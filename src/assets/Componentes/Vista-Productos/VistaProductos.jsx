2656a62d8448f0b9232e95ea981044a2d65fe907
MilagrosN <milagrosq682@gmail.com>
2025-10-25 18:02:10 -0300

    subiendo codigo en vistaproducto (esta vacio la tarjeta)

---
+++ src/assets/Componentes/Vista-Productos/VistaProductos.jsx
@@ -0,0 +1,28 @@
+import ProductCard from './TarjetaProductos.jsx';
+import './VistaProductos.css';
+
+function VistaProductos() {
+  const productos = [
+    { id: 1, nombre: 'Producto 1', precio: 10.99, imagen: 'ruta/a/imagen1.jpg' },
+    { id: 2, nombre: 'Producto 2', precio: 15.49, imagen: 'ruta/a/imagen2.jpg' },
+    { id: 3, nombre: 'Producto 3', precio: 7.99, imagen: 'ruta/a/imagen3.jpg' },
+    // Agrega más productos según sea necesario
+  ];    
+    return (
+    <div className="vista-productos">
+      <h1>Productos Disponibles</h1>
+      <div className="lista-productos"> 
+        {productos.map((producto) => (
+          <ProductCard  
+            key={producto.id}
+            nombre={producto.nombre}
+            precio={producto.precio}
+            imagen={producto.imagen}
+          />
+        ))}
+        </div> 
+    </div>
+    );
+}
+
+export default VistaProductos;
