a179105ddc7a96f16b54f1bdbc8c0f440bafa3c3
luciana <belenorosco50@gmail.com>
2025-10-29 20:20:47 -0300

    cambios  de vista productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import { Route, Routes, Switch } from 'wouter';
+import {Route, Routes, Switch} from 'wouter';
 import VistaProductos from './Componentes/Vista-Productos/VistaProductos';
-import VistaCarrito from './Componentes/Vista-Carrito/VistaCarrito';
+
 
 function App() {
   return (
-    
+
+    <Router>
       <Routes>
         <Route path="/" element={<VistaProductos />} />
         <Route path="/carrito" element={<VistaCarrito />} />
       </Routes>
-  )
+    </Router>
+
+  );
 }
+
 export default App;
