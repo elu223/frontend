28a17718a3c42a5b2c36fb2547c3be1b46279f7c
MilagrosN <milagrosq682@gmail.com>
2025-10-29 16:08:31 -0300

    conexion de los estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-import {Route, Routes, Switch} from 'wouter';
+import { Route, Routes, Switch } from 'wouter';
 import VistaProductos from './Componentes/Vista-Productos/VistaProductos';
 
 function App() {
   return (
-    <Switch>
+    
       <Routes>
         <Route path="/" element={<VistaProductos />} />
         <Route path="/carrito" element={<VistaCarrito />} />
       </Routes>
-    </Switch>
-  );
+  )
 }
-
 export default App;
