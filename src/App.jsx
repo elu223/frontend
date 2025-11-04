9da88875266d20b47742ed5b78153d33e45e268e
luciana <belenorosco50@gmail.com>
2025-11-04 19:28:10 -0300

    hola

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Route, Router, Routes } from 'wouter';
+import { Route, Router, Switch } from 'wouter';
 import VistaProductos from './Componentes/Vista-Productos/VistaProductos';
 import VistaCarrito from './Componentes/Vista-Carrito/VistaCarrito';
+import VistaProductoDetalle from './Componentes/Vista-Producto-Detalle/VistaProductoDetalle'
 import './App.css';
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
         <Switch>
           <Route path="/" component={VistaProductos} />
           <Route path="/carrito" component={VistaCarrito} />
-          {/* Agregar más rutas */}
+          <Route path="/producto/:id">{(params)=><VistaProductoDetalle id={params.id} />}</Route>
+          {/*más rutas */}
           <Route>404 - Página no encontrada</Route>
         </Switch>
       </div>
