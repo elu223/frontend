6fe43df8351f04a54ef906adaf5cb584ce32591b
luciana <belenorosco50@gmail.com>
2025-11-12 14:53:22 -0300

    Vista carrito funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,37 @@ import MiPerfil from './Componentes/MiPerfil/Mi-Perfil';
 import './App.css';
 
 function App() {
+  const [carrito, setCarrito] = useState([]);
+
+  const agregarAlCarrito = (producto) => {
+    setCarrito(prevCarrito => {
+      const productoExistente = prevCarrito.find(item => item.id === producto.id);
+      if (productoExistente) {
+        return prevCarrito.map(item =>
+          item.id === producto.id ? { ...item, cantidad: item.cantidad + 1 } : item
+        );
+      } else {
+        return [...prevCarrito, { ...producto, cantidad: 1 }];
+      }
+    });
+  };
 
   return (
     <Router>
       <div className="App">
         <Switch>
-          <Route path="/" component={VistaProductos} />
-          <Route path="/carrito" component={VistaCarrito} />
-          <Route path="/producto/:id" component={VistaProductoDetalle} />
+          <Route path="/">
+            <VistaProductos agregarAlCarrito={agregarAlCarrito} />
+          </Route>
+          <Route path="/carrito">
+            <VistaCarrito carrito={carrito} setCarrito={setCarrito} />
+          </Route>
+          <Route path="/producto/:id">
+            <VistaProductoDetalle agregarAlCarrito={agregarAlCarrito} />
+          </Route>
+          <Route path="/miperfil">
+            <MiPerfil />
+          </Route>
           {/*más rutas */}
           <Route>404 - Página no encontrada</Route>
         </Switch>
