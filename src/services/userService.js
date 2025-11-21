10bc0d2bf15b660d212e47246e4db992c46e12e3
MilagrosN <milagrosq682@gmail.com>
2025-11-21 17:33:07 -0300

    carpetas service

---
+++ src/services/userService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000";
+
+export const loginUser = (data) =>
+  axios.post(`${API_URL}/users/login`, data);
+
+export const registerUser = (data) =>
+  axios.post(`${API_URL}/users/register`, data);
+
+export const getUserProfile = (id) =>
+  axios.get(`${API_URL}/users/${id}`);
